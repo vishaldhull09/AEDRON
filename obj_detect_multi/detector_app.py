d91a2ae6e3800907a0fb81574f670594177ecc7d
Vishal dhull <47358116+vishaldhull09@users.noreply.github.com>
2020-08-01 22:37:11 +0530

    Update detector_app.py

--- obj_detect_multi/detector_app.py
+++ obj_detect_multi/detector_app.py
@@ -10,7 +10,7 @@ import numpy as np
 from queue import Queue
 from threading import Thread
 
-MODEL_BASE = '/home/vishal/Desktop/flask_app/realtimeCV/models-master/research'
+MODEL_BASE = r'/home/vishal/Desktop/flask_app/realtimeCV/models-master/research'
 sys.path.append(MODEL_BASE)
 sys.path.append(MODEL_BASE + '/object_detection')
 sys.path.append(MODEL_BASE + '/slim')
@@ -27,6 +27,8 @@ import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 from werkzeug.datastructures import CombinedMultiDict
@@ -38,9 +40,15 @@ app = Flask(__name__)
 
 
 
-
-PATH_TO_CKPT = '/home/vishal/Desktop/flask_app/realtimeCV/ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb'
+BASE_CKPT = '/home/vishal/Desktop/flask_app/realtimeCV/' 
+PATH_TO_CKPT = BASE_CKPT +  'ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb'
 PATH_TO_LABELS = MODEL_BASE + '/object_detection/data/mscoco_label_map.pbtxt'
+MODELS = {
+    'Mobilenet': 'ssd_mobilenet_v1_coco_2018_01_28',
+    'Inception': 'ssd_inception_v2_coco_2018_01_28',
+    'Resnet101': 'rfcn_resnet101_coco_2018_01_28',
+    'FasterRCNN_ResNet101': 'faster_rcnn_resnet101_coco_2018_01_28'
+}
 
 content_types = {'jpg': 'image/jpeg',
                  'jpeg': 'image/jpeg',
@@ -79,6 +87,7 @@ class FPS:
         # compute the (approximate) frames per second
         return self._numFrames / self.elapsed()
 
+
 class WebcamVideoStream:
     def __init__(self, src, width, height):
         # initialize the video camera stream and read the first frame
@@ -86,10 +95,6 @@ class WebcamVideoStream:
         self.src = src
         self.width = width
         self.height = height
-        #self.stream = cv2.VideoCapture(src)
-        #self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        #self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        #(self.grabbed, self.frame) = self.stream.read()
 
         # initialize the variable used to indicate if the thread should
         # be stopped
@@ -136,14 +141,17 @@ def is_image():
 
   return _is_image
 
+
 def draw_bounding_box_on_image(image, box, color='red', thickness=4):
   draw = ImageDraw.Draw(image)
   im_width, im_height = image.size
   ymin, xmin, ymax, xmax = box
   (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
                                 ymin * im_height, ymax * im_height)
+  print(left)
   draw.line([(left, top), (left, bottom), (right, bottom),
              (right, top), (left, top)], width=thickness, fill=color)
+
 
 def encode_image(image):
   image_buffer = io.BytesIO()
@@ -160,8 +168,8 @@ def encode_image(image):
 
 # Webcam feed Helper
 def worker(input_q, output_q):
-    detection_graph = client.detection_graph
-    sess = client.sess
+    detection_graph = model.detection_graph
+    sess = model.sess
     fps = FPS().start()
     while True:
         fps.update()
@@ -197,16 +205,15 @@ def detect_objects_webcam(image_np, sess, detection_graph):
         boxes=np.squeeze(boxes),
         classes=np.squeeze(classes).astype(np.int32),
         scores=np.squeeze(scores),
-        category_index=client.category_index,
+        category_index=model.category_index,
         min_score_thresh=.5
     )
     return dict(rect_points=rect_points, class_names=class_names, class_colors=class_colors)
 
 # Image class
 class PhotoForm(Form):
-  input_photo = FileField(
-      'File extension should be: %s (case-insensitive)' % ', '.join(extensions),
-      validators=[is_image()])
+  input_photo = FileField(validators=[is_image()])
+
 
 class VideoForm(Form):
     input_video = FileField()
@@ -214,12 +221,12 @@ class VideoForm(Form):
 # Obect Dection Class
 class ObjectDetector(object):
 
-  def __init__(self):
+  def __init__(self, PATH_TO_CKPT):
+    self.PATH_TO_CKPT = PATH_TO_CKPT
     self.detection_graph = self._build_graph()
     self.sess = tf.Session(graph=self.detection_graph)
 
     label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    #print(label_map)
     categories = label_map_util.convert_label_map_to_categories(
         label_map, max_num_classes=90, use_display_name=True)
     self.category_index = label_map_util.create_category_index(categories)
@@ -229,7 +236,7 @@ class ObjectDetector(object):
     detection_graph = tf.Graph()
     with detection_graph.as_default():
       od_graph_def = tf.GraphDef()
-      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+      with tf.gfile.GFile(self.PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
@@ -262,9 +269,9 @@ class ObjectDetector(object):
     return boxes, scores, classes.astype(int), num_detections.astype(int)
 
 # Detection function
-def detect_objects(image_path):
+def detect_objects(model, image_path):
   image = Image.open(image_path).convert('RGB')
-  boxes, scores, classes, num_detections = client.detect(image)
+  boxes, scores, classes, num_detections = model.detect(image)
   image.thumbnail((480, 480), Image.ANTIALIAS)
 
   new_images = {}
@@ -273,138 +280,133 @@ def detect_objects(image_path):
     cls = classes[i]
     if cls not in new_images.keys():
       new_images[cls] = image.copy()
+    print("drawing boxes")  
     draw_bounding_box_on_image(new_images[cls], boxes[i],
                                thickness=int(scores[i]*10)-4)
 
   result = {}
-  result['original'] = encode_image(image.copy())
+  #result['original'] = encode_image(image.copy())
+  result['detections'] = {}
+  result['detections']['original'] = encode_image(image.copy())
 
   for cls, new_image in new_images.items():
-    category = client.category_index[cls]['name']
-    result[category] = encode_image(new_image)
-  #print(result)
+    category = model.category_index[cls]['name']
+    #result[category] = encode_image(new_image)
+    result['detections'][category] = encode_image(new_image)
 
   return result
-
-
-
 
 
 @app.route('/')
 def main_display():
     photo_form = PhotoForm(request.form)
     video_form = VideoForm(request.form)
-    #return render_template('main.html', photo_form=photo_form, result={})
-    return render_template('main.html', photo_form=photo_form, video_form=video_form, result={})
-
-
-
+    return render_template('main.html', photo_form=photo_form, video_form=video_form, models=list(MODELS.keys()), result={})
 
 @app.route('/imgproc', methods=['GET', 'POST'])
 def imgproc():
-  video_form = VideoForm(request.form)
-  form = PhotoForm(CombinedMultiDict((request.files, request.form)))
-  if request.method == 'POST' and form.validate():
-    with tempfile.NamedTemporaryFile() as temp:
-      form.input_photo.data.save(temp)
-      temp.flush()
-      print(temp.name)
-      result = detect_objects(temp.name)
-      print(result.keys())
+    video_form = VideoForm(request.form)
+    form = PhotoForm(CombinedMultiDict((request.files, request.form)))
+    if request.method == 'POST' and form.validate():
+        with tempfile.NamedTemporaryFile(delete=False) as temp:
+            form.input_photo.data.save(temp)
+            temp.flush()
+            results = {}
+            print('detecting')
+            start_time = time.time();
+            for model_name, model_path in MODELS.items():
+                model_start_time = time.time()
+                PATH_TO_CKPT = BASE_CKPT + model_path + '/frozen_inference_graph.pb'
+                model = ObjectDetector(PATH_TO_CKPT)
+                results[model_name] = detect_objects(model, temp.name)
+                results[model_name]['time'] = round(time.time() - model_start_time, 2)
+            total_time = round(time.time() - start_time, 2)
+            print('detected')
+        photo_form = PhotoForm(request.form)
+        return render_template('main.html', photo_form=photo_form, video_form=video_form, models=list(MODELS.keys()), results=results, total_time=total_time)    
+    else:
+        return redirect(url_for('main_display'))
 
-    photo_form = PhotoForm(request.form)
-    return render_template('main.html',
-                           photo_form=photo_form, video_form=video_form, result=result)
-  else:
-    return redirect(url_for('main_display'))
 
 @app.route('/vidproc', methods=['GET', 'POST'])
 def vidproc():
-    print("In vidproc")
+    global PATH_TO_CKPT
+    print('vidproc')
+   
     form = VideoForm(CombinedMultiDict((request.files, request.form)))
     if request.method == 'POST':
-        print("vid sub")
+        PATH_TO_CKPT = BASE_CKPT +  MODELS[request.form['model']] + '/frozen_inference_graph.pb'
         with tempfile.NamedTemporaryFile(delete=False) as temp:
             form.input_video.data.save(temp)
             temp.flush()
             session['vid'] = temp.name
-        return render_template('video.html')
+        video_form = VideoForm(request.form)
+        photo_form = PhotoForm(request.form)
+
+        return render_template('video.html', photo_form=photo_form, video_form=video_form, models=list(MODELS.keys()), video=True)
 
 
+foundClasses = []
 
-
+@app.route('/temp')
+def temp():
+    return jsonify(foundClasses)
 
 @app.route('/vidpros')
 def vidpros():
-    graph = client.detection_graph
+    model = ObjectDetector(PATH_TO_CKPT)
+    global foundClasses
+    foundClasses = []
+    graph = model.detection_graph
     image_tensor = graph.get_tensor_by_name('image_tensor:0')
     boxes = graph.get_tensor_by_name('detection_boxes:0')
     scores = graph.get_tensor_by_name('detection_scores:0')
     classes = graph.get_tensor_by_name('detection_classes:0')
     num_detections = graph.get_tensor_by_name('num_detections:0')
     
-
-
     vid_source = cv2.VideoCapture(session['vid'])
-    print("vid src")
     
     def generate(image_tensor, boxes, scores, classes, num_detections):
         ret, frame = vid_source.read()
-        #label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-	    #print(label_map)
-	    #categories = label_map_util.convert_label_map_to_categories(
-	     #   label_map, max_num_classes=90, use_display_name=True) 
-	    #category_index = label_map_util.create_category_index(categories)
-        # tensor code
-        result2={}
-        index=0
         while ret:
-            #image_np = client._load_image_into_numpy_array(frame)
-            #result2=[]
             image_np_expanded = np.expand_dims(frame, axis=0)
 
-            (boxes_t, scores_t, classes_t, num_detections_t) = client.sess.run(
+            (boxes_t, scores_t, classes_t, num_detections_t) = model.sess.run(
                 [boxes, scores, classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
 
-            im,clas=vis_util.visualize_boxes_and_labels_on_image_array(
+            boxes_t, scores_t, classes_t, num_detections_t = map(np.squeeze, [boxes_t, scores_t, classes_t, num_detections_t])
+
+            for i in range(num_detections_t.astype(int)):
+                if scores_t[i] < 0.5: continue
+                foundClass = model.category_index[classes_t[i]]['name']
+                if foundClass not in foundClasses:
+                    foundClasses.append(foundClass)
+
+
+            vis_util.visualize_boxes_and_labels_on_image_array(
             frame,
-            np.squeeze(boxes_t),
-            np.squeeze(classes_t).astype(np.int32),
-            np.squeeze(scores_t),
-            client.category_index,
+            boxes_t,
+            classes_t.astype(np.int32),
+            scores_t,
+            model.category_index,
             use_normalized_coordinates=True,
             line_thickness=8)
-
-            #print(display_str)
-            #image_pil = Image.fromarray(np.uint8(frame)).convert('RGB')
-            #result2.append(set(classes_t[np.where(classes_t>1)]))
-            if len(clas)>0 and (clas not in result2.values()): 
-                 result2[index]= list(clas)
-
-
-            #if(max(classes_t).any()>1):
-            #	result2.append(int(max(classes_t)))
-            #	print(result2)
-            	#print(result2)
-
+            
             payload = cv2.imencode('.jpg', frame)[1].tobytes()
+
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + payload + b'\r\n')
-            ret, frame = vid_source.read()
-            index+=1
-        #print(result2)   
-       
 
-    print("Before return")
-    #result3=[]
-    #result3=generate(image_tensor, boxes, scores, classes, num_detections)
-    #print(list(result3))
+            ret, frame = vid_source.read()
+        print(foundClasses)
+    
     return Response(generate(image_tensor, boxes, scores, classes, num_detections), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/json',methods=['GET'])
 def json():
-    graph = client.detection_graph
+    graph = model.detection_graph
     image_tensor = graph.get_tensor_by_name('image_tensor:0')
     boxes = graph.get_tensor_by_name('detection_boxes:0')
     scores = graph.get_tensor_by_name('detection_scores:0')
@@ -427,11 +429,11 @@ def json():
         result2={}
         index=0
         while ret:
-            #image_np = client._load_image_into_numpy_array(frame)
+            #image_np = model._load_image_into_numpy_array(frame)
             #result2=[]
             image_np_expanded = np.expand_dims(frame, axis=0)
 
-            (boxes_t, scores_t, classes_t, num_detections_t) = client.sess.run(
+            (boxes_t, scores_t, classes_t, num_detections_t) = model.sess.run(
                 [boxes, scores, classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
 
@@ -440,7 +442,7 @@ def json():
             np.squeeze(boxes_t),
             np.squeeze(classes_t).astype(np.int32),
             np.squeeze(scores_t),
-            client.category_index,
+            model.category_index,
             use_normalized_coordinates=True,
             line_thickness=8)
 
@@ -472,11 +474,10 @@ def json():
     return jsonify(result3)
 
 
-
-
 @app.route('/realproc', methods=['GET', 'POST'])
 def realproc():
     return render_template('realtime.html')
+
 
 @app.route('/realstop', methods=['GET', 'POST'])
 def realstop():
@@ -542,19 +543,17 @@ def realpros():
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-client = ObjectDetector()
-
-video_init = WebcamVideoStream(src=1, width=480, height=360)
+#import time
+#start_time = time.time()
+#model = ObjectDetector(PATH_TO_CKPT)
+#print('model took', time.time() - start_time)
+video_init = WebcamVideoStream(src=0, width=480, height=360)
 fps_init = FPS()
 
 app.secret_key = 'super secret key'
-app.config['SESSION_TYPE'] = 'filesystem'
 
 if __name__ == '__main__':
 
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-
-    sess.init_app(app)
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(debug=True)
